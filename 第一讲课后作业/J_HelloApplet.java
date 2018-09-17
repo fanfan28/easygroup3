c7ea8b923bbefc9c45c5287429314b97ee8a7155
范钟元 <cs17bff@163.com>
2018-09-17 14:14:36 +0800

    Upload J_HelloApplet.java AppletExample.html J_HelloJava.java

---
+++ 第一讲课后作业/J_HelloApplet.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
+public class J_HelloApplet extends JApplet
+{
+    public void paint(Graphics g)
+    {
+        g.clearRect(0, 0, getWidth( ), getHeight( )); 
+        g.drawString("小应用程序，您好!", 10, 20);
+    } 
+} 
